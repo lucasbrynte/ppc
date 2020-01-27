5b13fcc8d404b2fbb8108f83879f05a74d0f1277
Lucas Brynte <brynte@chalmers.se>
2020-01-27 09:57:08 +0100

    numpy->pytorch torchvision mean & std

--- lib/constants.py
+++ lib/constants.py
@@ -1,6 +1,6 @@
 """Constants for 3DOD."""
 import os
-import numpy as np
+import torch
 
 # Execution modes
 TRAIN = 'train'
@@ -12,8 +12,8 @@ PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
 CONFIG_ROOT = os.path.join(PROJECT_PATH, 'conf')
 
 # TorchVision
-TV_MEAN = 255.*np.array((0.485, 0.456, 0.406))
-TV_STD = 255.*np.array((0.229, 0.224, 0.225))
+TV_MEAN = 255.*torch.tensor((0.485, 0.456, 0.406))
+TV_STD = 255.*torch.tensor((0.229, 0.224, 0.225))
 
 # Matplotlib
 PYPLOT_DPI = 100
